9afab0533c53692e8c000d10ff0fdef775e1421e
eghadirian <60559670+eghadirian@users.noreply.github.com>
2021-07-08 20:56:06 -0400

    smiles sequencing using LSTM

---
+++ P20-SMILES_Sequencing.py
@@ -0,0 +1,242 @@
+! pip install rdkit-pypi
+
+! wget 'https://raw.githubusercontent.com/chaninlab/qsar/main/gdb11_size08.smi'
+
+import os
+import pandas as pd
+import numpy as np
+from rdkit import Chem
+from rdkit.Chem import Draw, Descriptors
+from matplotlib import pyplot as plt
+from sklearn.model_selection import train_test_split
+
+smifile = "gdb11_size08.smi"
+data = pd.read_csv(smifile, delimiter = "\t", names = ["smiles","No","Int"])
+smiles_train, smiles_test = train_test_split(data["smiles"], random_state=42)
+print((smiles_train.shape))
+print((smiles_test.shape))
+
+def vectorize(smiles):
+        one_hot =  np.zeros((smiles.shape[0], embed , len(charset)), dtype=np.int8)
+        for i,smile in enumerate(smiles):
+            #encode the startchar
+            one_hot[i,0,char_to_int["!"]] = 1
+            #encode the rest of the chars
+            for j,c in enumerate(smile):
+                one_hot[i,j+1,char_to_int[c]] = 1
+            #Encode endchar
+            one_hot[i,len(smile)+1:,char_to_int["E"]] = 1
+        #Return two, one for input and the other for output
+        return one_hot[:,0:-1,:], one_hot[:,1:,:]
+X_train, Y_train = vectorize(smiles_train.values)
+X_test, Y_test = vectorize(smiles_test.values)
+print(smiles_train.iloc[0])
+plt.matshow(X_train[0].T)
+
+print("".join([int_to_char[idx] for idx in np.argmax(X_train[0,:,:], axis=1)]))
+
+import tensorflow.keras as keras
+from keras.models import Model
+from keras.layers import Input
+from keras.layers import LSTM
+from keras.layers import Dense
+from keras.layers import Concatenate
+from keras import regularizers
+input_shape = X_train.shape[1:]
+output_dim = Y_train.shape[-1]
+latent_dim = 64
+lstm_dim = 64
+
+unroll = False
+encoder_inputs = Input(shape=input_shape)
+encoder = LSTM(lstm_dim, return_state=True,
+                unroll=unroll)
+encoder_outputs, state_h, state_c = encoder(encoder_inputs)
+states = Concatenate(axis=-1)([state_h, state_c])
+neck = Dense(latent_dim, activation="relu")
+neck_outputs = neck(states)
+
+decode_h = Dense(lstm_dim, activation="relu")
+decode_c = Dense(lstm_dim, activation="relu")
+state_h_decoded =  decode_h(neck_outputs)
+state_c_decoded =  decode_c(neck_outputs)
+encoder_states = [state_h_decoded, state_c_decoded]
+decoder_inputs = Input(shape=input_shape)
+decoder_lstm = LSTM(lstm_dim,
+                    return_sequences=True,
+                    unroll=unroll
+                   )
+decoder_outputs = decoder_lstm(decoder_inputs, initial_state=encoder_states)
+decoder_dense = Dense(output_dim, activation='softmax')
+decoder_outputs = decoder_dense(decoder_outputs)
+#Define the model, that inputs the training vector for two places, and predicts one character ahead of the input
+model = Model([encoder_inputs, decoder_inputs], decoder_outputs)
+print(model.summary())
+
+from keras.callbacks import History, ReduceLROnPlateau
+h = History()
+rlr = ReduceLROnPlateau(monitor='val_loss', factor=0.5,\
+    patience=10, min_lr=0.000001, verbose=1, min_delta=1e-5)
+
+model.fit([X_train, X_train], Y_train,
+                    epochs=200,
+                    batch_size=256,
+                    callbacks=[h, rlr],
+                    validation_split=0.1
+                    )
+
+plt.plot(h.history["loss"], label="Loss")
+plt.plot(h.history["val_loss"], label="Loss")
+plt.yscale("log")
+plt.legend()
+
+for i in range(100):
+    v = model.predict([X_test[i:i+1], X_test[i:i+1]]) #Can't be done as output not necessarely 1
+    idxs = np.argmax(v, axis=2)
+    pred=  "".join([int_to_char[h] for h in idxs[0]])[:-1]
+    idxs2 = np.argmax(X_test[i:i+1], axis=2)
+    true =  "".join([int_to_char[k] for k in idxs2[0]])[1:]
+    if true != pred:
+        print(true, pred)
+
+smiles_to_latent_model = Model(encoder_inputs, neck_outputs)
+smiles_to_latent_model.save("Blog_simple_smi2lat.h5")
+
+latent_input = Input(shape=(latent_dim,))
+#reuse_layers
+state_h_decoded_2 =  decode_h(latent_input)
+state_c_decoded_2 =  decode_c(latent_input)
+latent_to_states_model = Model(latent_input, [state_h_decoded_2, state_c_decoded_2])
+latent_to_states_model.save("Blog_simple_lat2state.h5")
+
+#Last one is special, we need to change it to stateful, and change the input shape
+inf_decoder_inputs = Input(batch_shape=(1, 1, input_shape[1]))
+inf_decoder_lstm = LSTM(lstm_dim,
+                    return_sequences=True,
+                    unroll=unroll,
+                    stateful=True
+                   )
+inf_decoder_outputs = inf_decoder_lstm(inf_decoder_inputs)
+inf_decoder_dense = Dense(output_dim, activation='softmax')
+inf_decoder_outputs = inf_decoder_dense(inf_decoder_outputs)
+sample_model = Model(inf_decoder_inputs, inf_decoder_outputs)
+
+#Transfer Weights
+for i in range(1,3):
+    sample_model.layers[i].set_weights(model.layers[i+6].get_weights())
+sample_model.save("Blog_simple_samplemodel.h5")
+
+sample_model.summary()
+
+x_latent = smiles_to_latent_model.predict(X_test)
+
+molno = 5
+latent_mol = smiles_to_latent_model.predict(X_test[molno:molno+1])
+sorti = np.argsort(np.sum(np.abs(x_latent - latent_mol), axis=1))
+print(sorti[0:10])
+print(smiles_test.iloc[sorti[0:8]])
+Draw.MolsToImage(smiles_test.iloc[sorti[0:8]].apply(Chem.MolFromSmiles))
+
+Draw.MolsToImage(smiles_test.iloc[sorti[-8:]].apply(Chem.MolFromSmiles))
+
+logp = smiles_test.apply(Chem.MolFromSmiles).apply(Descriptors.MolLogP)
+
+from sklearn.decomposition import PCA
+pca = PCA(n_components = 2)
+red = pca.fit_transform(x_latent)
+plt.figure()
+plt.scatter(red[:,0], red[:,1],marker='.', c= logp)
+print(pca.explained_variance_ratio_, np.sum(pca.explained_variance_ratio_))
+
+molwt = smiles_test.apply(Chem.MolFromSmiles).apply(Descriptors.MolMR)
+plt.figure()
+plt.scatter(red[:,0], red[:,1],marker='.', c= molwt)
+
+#Model LogP?
+x_train_latent = smiles_to_latent_model.predict(X_train)
+logp_train = smiles_train.apply(Chem.MolFromSmiles).apply(Descriptors.MolLogP)
+
+from keras.models import Sequential
+logp_model = Sequential()
+logp_model.add(Dense(128, input_shape=(latent_dim,), activation="relu"))
+logp_model.add(Dense(128, activation="relu"))
+logp_model.add(Dense(1))
+logp_model.compile(optimizer="adam", loss="mse")
+
+rlr = ReduceLROnPlateau(monitor='val_loss', factor=0.5,patience=10, min_lr=0.000001, verbose=1, epsilon=1e-5)
+logp_model.fit(x_train_latent, logp_train, batch_size=128, epochs=400, callbacks = [rlr])
+
+logp_pred_train = logp_model.predict(x_train_latent)
+logp_pred_test = logp_model.predict(x_latent)
+plt.scatter(logp, logp_pred_test, label="Test")
+plt.scatter(logp_train, logp_pred_train, label="Train")
+plt.legend()
+
+def latent_to_smiles(latent):
+    #decode states and set Reset the LSTM cells with them
+    states = latent_to_states_model.predict(latent)
+    sample_model.layers[1].reset_states(states=[states[0],states[1]])
+    #Prepare the input char
+    startidx = char_to_int["!"]
+    samplevec = np.zeros((1,1,22))
+    samplevec[0,0,startidx] = 1
+    smiles = ""
+    #Loop and predict next char
+    for i in range(28):
+        o = sample_model.predict(samplevec)
+        sampleidx = np.argmax(o)
+        samplechar = int_to_char[sampleidx]
+        if samplechar != "E":
+            smiles = smiles + int_to_char[sampleidx]
+            samplevec = np.zeros((1,1,22))
+            samplevec[0,0,sampleidx] = 1
+        else:
+            break
+    return smiles
+
+smiles = latent_to_smiles(x_latent[0:1])
+print(smiles)
+print(smiles_test.iloc[0])
+
+wrong = 0
+for i in range(1000):
+    smiles = latent_to_smiles(x_latent[i:i+1])
+    mol = Chem.MolFromSmiles(smiles)
+    if mol:
+        pass
+    else:
+        print(smiles)
+        wrong = wrong + 1
+print('{} percent wrongly formatted smiles'.format(wrong/float(1000)*100))
+
+#Interpolation test in latent_space
+i = 0
+j= 2
+latent1 = x_latent[j:j+1]
+latent0 = x_latent[i:i+1]
+mols1 = []
+ratios = np.linspace(0,1,25)
+for r in ratios:
+    #print r
+    rlatent = (1.0-r)*latent0 + r*latent1
+    smiles  = latent_to_smiles(rlatent)
+    mol = Chem.MolFromSmiles(smiles)
+    if mol:
+        mols1.append(mol)
+    else:
+        print(smiles)
+Draw.MolsToGridImage(mols1, molsPerRow=5)
+
+#Sample around the latent wector
+latent = x_latent[0:1]
+scale = 0.40
+mols = []
+for i in range(20):
+    latent_r = latent + scale*(np.random.randn(latent.shape[1])) #TODO, try with
+    smiles = latent_to_smiles(latent_r)
+    mol = Chem.MolFromSmiles(smiles)
+    if mol:
+        mols.append(mol)
+    else:
+        print(smiles)
+Draw.MolsToGridImage(mols, molsPerRow=5)
